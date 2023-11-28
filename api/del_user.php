fb54837e60dfbc2929733fb3809aefdb5aee6e27
ren1023 <renyou.1023@gmail.com>
2023-11-28 09:45:54 +0800

    從11202_php中拆分出來

---
+++ api/del_user.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once "../include/connect.php";
+
+// $sql="delete from `users` where `id`= '{$_GET['id']}'";
+
+del('users',$_GET['id']);
+// $pdo->exec($sql);
+
+
+unset($_SESSION['user']);
+header('location:../index.php');
+
+?>
